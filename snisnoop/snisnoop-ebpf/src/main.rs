1903a89242e98fc06ccdc954a3ae425c89f68107
Joshua Koo <zz85nus@gmail.com>
2025-05-04 03:12:16 +0000

    Add snisnoop using aya-template
    - using `cargo generate https://github.com/aya-rs/aya-template`
    - sets up classifier ebpf program

---
+++ snisnoop/snisnoop-ebpf/src/main.rs
@@ -0,0 +1,28 @@
+#![no_std]
+#![no_main]
+
+use aya_ebpf::{bindings::TC_ACT_PIPE, macros::classifier, programs::TcContext};
+use aya_log_ebpf::info;
+
+#[classifier]
+pub fn snisnoop(ctx: TcContext) -> i32 {
+    match try_snisnoop(ctx) {
+        Ok(ret) => ret,
+        Err(ret) => ret,
+    }
+}
+
+fn try_snisnoop(ctx: TcContext) -> Result<i32, i32> {
+    info!(&ctx, "received a packet");
+    Ok(TC_ACT_PIPE)
+}
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+#[link_section = "license"]
+#[no_mangle]
+static LICENSE: [u8; 13] = *b"Dual MIT/GPL\0";
